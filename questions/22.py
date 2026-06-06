b83060cdab8ce0de14d15a2cb8e45d5751dcde8d
Allowwed <surendranathdas24@gmail.com>
2026-06-06 17:28:54 +0530

    updated 22.py

--- questions/22.py
+++ questions/22.py
@@ -9,5 +9,4 @@ df = pd.DataFrame(data)
 
 df.to_csv("student_data.csv", index=False)
 
-
 # (OUTPUT) This code saves the DataFrame to a CSV file named "student_data.csv" in the current working directory. The index=False parameter is used to prevent the index from being written to the CSV file.
